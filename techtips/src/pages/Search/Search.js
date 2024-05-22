988bd4f5fb435a4e1b1bc5ebb80bc6568e837a29
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-05-22 20:20:04 -0300

    Update search

--- techtips/src/pages/Search/Search.js
+++ techtips/src/pages/Search/Search.js
@@ -1,14 +1,32 @@
+import styles from './Search.module.css'
+
 //hooks
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
+
+//components
+import { PostDetail } from "../../components/PostDetail";
+
+import { Link } from "react-router-dom";
 
 export const Search = () => {
     const query = useQuery();
     const search = query.get("q");
 
+    const {documents: posts} = useFetchDocuments("posts", search)
+
     return (
-        <div>
-        <p>{search}</p>
+        <div className={styles.search_container}>
+            <h1>Search</h1>
+            {posts && posts.length === 0 && (
+                <div className={styles.noposts}>
+                    <p>Não foram encontrados posts a partir da sua busca...</p>
+                    <Link to="/" className="btn btn-dark">Voltar</Link>
+                </div>
+            )}
+            {posts && posts.map((post) => (
+                <PostDetail key={post.id} post={post}/>
+            ))}
         </div>
     )
 }
